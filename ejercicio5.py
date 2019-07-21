cab76ab345e850df3c05d652757e48707e85e6e9
franz lopez <franzelunico@gmail.com>
2019-07-20 20:44:57 -0400

    revisado

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,3 +1,4 @@
+# ok
 #Escriba una función que pida al usuario dos números enteros, y luego entregue la suma de todos los números que están #entre ellos. Por ejemplo, si los números son 1 y 7, debe entregar como resultado 2 + 3 + 4 + 5 + 6 = 20.
 #Ingrese num: 1
 #Ingrese num: 7
