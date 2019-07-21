f4f21283e5fcd81eeea0ca75d2c1ee36d8fbd7df
franz lopez <franzelunico@gmail.com>
2019-07-20 20:11:48 -0400

    cambio de ISO-8859-2 a UTF-8.

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,6 @@
-#Invertir un número ( Ejm: entrada = 123, salida=321).
+#Invertir un nÃºmero ( Ejm: entrada = 123, salida=321).
 
-numero_ingresado = (input("Ingrese un número: "))
+numero_ingresado = (input("Ingrese un nÃºmero: "))
 revertir = 0
 try:
     valor = int(numero_ingresado)
@@ -8,6 +8,6 @@ try:
         residuo = valor % 10
         revertir = (revertir * 10) + residuo
         valor //= 10
-    print('El inverso del número ingresado es: ', revertir)
+    print('El inverso del nÃºmero ingresado es: ', revertir)
 except ValueError:
     print("Numero no valido. Ingresar de nuevo!")
