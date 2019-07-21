36adea037333c3a8a28ca30bd8a298c4d7869de7
franz lopez <franzelunico@gmail.com>
2019-07-20 20:26:27 -0400

    revisado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,14 @@
+# ok
 #Calcule el promedio de un curso (Ejm entrada [70,80,90,100], salida=85).
-lista = [70,80,90,100]
-tamańoLista = len(lista)
-lista2 = 0
-for i in range(tamańoLista):
-	lista2 = lista2+lista[i]
-lista2 = lista2/tamańoLista 
+
+def get_promedio_notas(lista):
+    tamLista = len(lista)
+    lista2 = 0
+    for i in range(tamLista):
+        lista2 = lista2+lista[i]
+    lista2 = lista2/tamLista
+    return lista2
+
+lista_notas = [70,80,90,100]
+promedio = get_promedio_notas(lista_notas)
+print(promedio)
