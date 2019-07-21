d82eccbe16a442342ab4b48148ab114598b50f64
franz lopez <franzelunico@gmail.com>
2019-07-20 20:42:15 -0400

    revisado

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,9 +1,10 @@
+# ok
 #Escriba el teorema de pitágoras que reciba como entrada las longitudes de los dos catetos a y b , que #entregue como salida el largo de la hipotenusa c del triángulo (Ejm a = 7, b = 5, salida=8.60).
 
 import math
-#import math.sqrt()
+
 print("\t Teorema de Pitagoras\n")
-a=input("Ingrese cateto a: ")
-b=input("Ingrese cateto b: ")
-c=math.sqrt((a*a)+(b*b ))
-print("La hipotenusa es:", c)
+a=float(input("Ingrese cateto a: "))
+b=float(input("Ingrese cateto b: "))
+c=math.sqrt((a**2)+(b**2 ))
+print('La hipotenusa es: {0:.2f}'.format(c))
