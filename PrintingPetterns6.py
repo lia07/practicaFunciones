a6201bd8aee9e1c0b9384e8dbabc3932b32cb0da
lia07 <sarzurisilvestre@example.com>
2020-07-09 14:53:32 +0200

    ej6

--- PrintingPetterns6.py
+++ PrintingPetterns6.py
@@ -1,9 +1,9 @@
 def pattern(n):
     k = 2*n-2
-    for i in range(0,n):
-        for j in range(0, k):
+    for i in range(n,-1,-1):
+        for j in range(k, 0, -1):
             print(end="")
-        k=k-1
+        k=k+1
         for j in range(0,i+1):
             print("*", end="")
         print("\r")
